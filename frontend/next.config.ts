8e3dcc054bad7226982eb2887536ba9eee123144
Thiago Gomes <thiago.gomes@sou.inteli.edu.br>
2025-12-01 03:24:28 -0300

    fix: corrige erros no next.config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,12 +5,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   
   // Modo Standalone para o deploy funcionar corretamente no Amplify
-  output: 'standalone',
+  // output: 'standalone',
   
   // --- IMAGENS ---
   images: {
